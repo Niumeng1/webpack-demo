d4b916246228f6f9a28793500dda6e794ec8f81c
snowflake <875347807@qq.com>
2018-11-09 16:39:12 +0800

    管理资源

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
  import _ from 'lodash';
+ import './style.css';
+ import icon from './icon.jpg';
+ import Data from './data.xml';
 
   function component() {
     var element = document.createElement('div');
@@ -6,6 +9,15 @@
    // Lodash, currently included via a script, is required for this line to work
    // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+
+    //将图像添加到我们现有的div中.
+    let myIcon = new Image();
+    myIcon.src = icon;
+
+    element.appendChild(myIcon);
+
+    console.log(Data);
 
     return element;
   }
--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,40 @@ module.exports = {
     output : {
         filename : "bundle.js",
         path : path.resolve (__dirname , 'dist')
+    },
+    module: {
+        rules:[
+            {
+                test: /\.css$/,  //webpack 根据正则表达式，来确定应该查找哪些文件，并将其提供给指定的 loader。在这种情况下，以 .css 结尾的全部文件，都将被提供给 style-loader 和 css-loader。
+                use : [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/, //以 .png... 结尾的全部文件，都将被提供给 file-loader。
+                use : [
+                    'file-loader'
+                ] 
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,//以 .woff... 结尾的全部文件，都将被提供给 file-loader。
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(csv|tsv)$/,
+                use: [
+                    'csv-loader'
+                ]
+            },
+            {
+                test: /\.xml$/,
+                use: [
+                    'xml-loader'
+                ]
+            }
+        ]
     }
 };
