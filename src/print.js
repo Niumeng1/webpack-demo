d4eab83b7061fb5efddd4a9a96dd0478ef69952a
snowflake <875347807@qq.com>
2018-11-09 18:11:03 +0800

    开发服务器
    1.使用source-map
    2.选择一个开发工具
    3.使用观察模式
    4.使用webpack-dev-server
    5.使用webpack-dev-middleware
    6.调整文本编辑器

--- src/print.js
+++ src/print.js
@@ -1,3 +1,4 @@
 export default function printMe() {
-    console.log('I get called form pring.js');
+   // console.log('I get called form pring.js');
+   console.error('I get called form print.js!');
 }
